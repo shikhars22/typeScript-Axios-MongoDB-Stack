2cb899a15d4678924064e93c582e8192aa87aa9b
shikhars22 <shikhars22@gmail.com>
2022-12-19 16:01:44 +0530

    Lesson 57 done

--- cryptocurrencies/src/App.tsx
+++ cryptocurrencies/src/App.tsx
@@ -3,16 +3,54 @@ import './App.css';
 import axios from 'axios';
 import CryptoSummary from './components/CryptoSummary';
 import { Crypto } from './Types';
+import type { ChartData, ChartOptions } from 'chart.js';
+import React from 'react';
+
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import moment from 'moment';
+
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend
+);
 
 function App() {
 	const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
 	const [selected, setSelected] = useState<Crypto | null>();
+	const [data, setData] = useState<ChartData<'line'>>();
+	const [options, setOptions] = useState<ChartOptions<'line'>>({
+		responsive: true,
+		plugins: {
+			legend: {
+				position: 'top' as const,
+			},
+			title: {
+				display: true,
+				text: 'Chart.js Line Chart',
+			},
+		},
+	});
 
 	useEffect(() => {
 		const url =
 			'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false';
 		axios.get(url).then((response) => {
-			console.log(response.data);
+			// console.log(response.data);
 			setCryptos(response.data);
 		});
 	}, []);
@@ -22,10 +60,36 @@ function App() {
 				<h2>Here are the Cryptos</h2>
 				<select
 					onChange={(e) => {
-						console.log(e.target.value);
+						// console.log(e.target.value);
 						const c = cryptos?.find((x) => x.id === e.target.value);
 						setSelected(c);
-						console.log(c);
+						// console.log(c);
+						axios
+							.get(
+								`https://api.coingecko.com/api/v3/coins/${c?.id}/market_chart?vs_currency=inr&days=30&interval=daily`
+							)
+							.then((response) => {
+								console.log(response.data);
+								setData({
+									labels: response.data.prices.map((price: number[]) => {
+										return moment.unix(price[0] / 1000).format('DD-MM-YYYY');
+									}),
+									datasets: [
+										{
+											label: 'Dataset 1',
+											data: response.data.prices.map((price: number[]) => {
+												return price[1];
+											}),
+											borderColor: 'rgb(255, 99, 132)',
+											backgroundColor: 'rgba(255, 99, 132, 0.5)',
+										},
+									],
+								});
+								// console.log(data);
+							})
+							.catch((e) => {
+								console.log(e);
+							});
 					}}
 					defaultValue='default'>
 					<optgroup label='Default'>
@@ -48,6 +112,14 @@ function App() {
 				</select>
 			</div>
 			{selected ? <CryptoSummary crypto={selected} /> : null}
+			{data ? (
+				<div style={{ width: 600 }}>
+					<Line
+						options={options}
+						data={data}
+					/>
+				</div>
+			) : null}
 		</>
 	);
 }
